43430836d39825600c573419cf89d3917d306722
shekhar-2410 <shekhar.devanshee@gmail.com>
2022-06-04 11:27:51 +0530

    tender-ux social-icons

--- src/app/views/share/tender/tender.component.ts
+++ src/app/views/share/tender/tender.component.ts
@@ -9,10 +9,19 @@ export class TenderComponent implements OnInit {
 	@Input() item: any;
 	pageData: any;
 
+	active_tender: any = [];
+	archive_tender: any = [];
 	constructor() {}
 
 	ngOnInit(): void {
 		this.pageData = this.item;
+		if (this.item && this.item.component_data) {
+			this.item.component_data.forEach((e: any, i: any) => {
+				if (e.is_active) {
+					this.active_tender.push(e);
+				} else this.archive_tender.push(e);
+			});
+		}
 		console.log('tender', this.pageData);
 	}
 }
